3598492995c5f8e3254a0a6133e677192fe40002
anandhu <aajayakumar@menervasoftware.com>
2019-11-24 12:47:48 +0530

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: './modules/chat/chat.module#ChatModule', pathMatch: 'full' },
+  { path: 'login', loadChildren: './modules/login/login.module#LoginModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
